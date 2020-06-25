eec3eab29d6246ae46840eb98f57a1efac6070d2
Brutos212 <brutos212@gmail.com>
2020-06-25 12:08:25 -0300

    exemplo basico de HTTP REST test

--- k_Py/pong.py
+++ k_Py/pong.py
@@ -14,6 +14,7 @@ class PongBallWidget(Widget):
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
+
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
 
---
+++ rest_tester/rest_tester.py
@@ -0,0 +1,45 @@
+# Performe HTTPS resquests and print responses
+# URL
+# Dropdown: GET, POST, PUT, PATCH, DELET
+# Response Body:]
+import requests
+from kivy.app import App
+from kivy.uix.dropdown import DropDown
+from kivy.uix.widget import Widget
+
+
+class HttpMethodDropDown(DropDown):
+    def select(self, data):
+        print("Dropdown option was selected: %s" % data)
+
+
+class BaseWindow(Widget):
+
+    method = None
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def perform_http_request(self, method):
+        # TODO predefine the request, update the method accordingly
+
+        request = requests.Request(method, url=self.url.text)
+        response = requests.Session().send(request.prepare())
+        self.response_body.text = response.text
+
+        # print(self.url.text)
+        # def perform_http_Request
+        # Get the URL from the textfield
+        # perform request
+        # put output in a result field
+
+
+class WebtesterApp(App):
+    def build(self):
+        base_window = BaseWindow()
+        return base_window
+
+
+if __name__ == '__main__':
+    webtester = WebtesterApp()
+    webtester.run()
